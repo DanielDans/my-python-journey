f650f941f6950df282ffba4b35e84a8228cead96
Maple <topginkyliquid@gmail.com>
2026-08-18 15:02:42 +0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,26 @@
+# mathematical operations
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4)
+print(3 // 4)
+print(3 ** 4)
+# strings 
+# 'sumo'
+# 'vn'
+# '30 python'
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type({3.4, 4.5}))
+print(type('dumbass time taking shit bro'))
+# exercise 3
+# define coordinates as (x, y)
+point1 = (2, 3)
+point2 = (10, 8)
+# formula: square root of (x2 - x1)^2 + (y2 - y1)^2
+distance = ((point2[0] - point1[0])**2 + ((point2[1] - point1[1])**2))** 0.5
+print(distance)
