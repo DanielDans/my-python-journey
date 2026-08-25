ba8e89a0a7310b74299834fce11036bbe03fe254
Maple <topginkyliquid@gmail.com>
2026-08-25 21:22:24 +0700

    a filler file, doesnt have a lot

---
+++ day7.py
@@ -0,0 +1,5 @@
+i = 4
+while i != 0: # works a bit like if except it loops
+    print('ohio') # action on every loop
+    i = i - 1 # this changes the value of i every loop so that the loop is finite
+    # infinite loops kinda just not work here
